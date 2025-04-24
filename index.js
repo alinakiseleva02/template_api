35f57a0f30d13849338f478f26ab3e9781e4ea9a
Студент группы IS <is@sielom.local>
2025-04-24 17:48:53 +0500

    Вывод данных изх таблицы API

--- index.js
+++ index.js
@@ -5,24 +5,16 @@ const port = 3000
 
 const { createClient } = require ('@supabase/supabase-js')
 
-const supabaseUrl = 'https://ebfpllkgtaxgrqxjlclq.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImViZnBsbGtndGF4Z3JxeGpsY2xxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ3ODEwMTcsImV4cCI6MjA2MDM1NzAxN30.tYaiFSq9GdJDexYSRTS50Cu_V0c4WFg-KkeFGAhcy4Y'
+const supabaseUrl = 'https://zzfrbvhfvxdfeuotcdnh.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp6ZnJidmhmdnhkZmV1b3RjZG5oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI0NjU1MDIsImV4cCI6MjA0ODA0MTUwMn0.kupSy-3Qwqm1CYCXVCxKYFkrDFzv3ghhTEERREpxhLc'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-app.get('/students', (req, res) => {
-        res.send('Список студентов');
-    })
-
-    app.post('/students', (req, res) => {
-        res.send('Добавление студентов');
-    })
-
-    app.put('/students', (req, res) => {
-        res.send('Изменение студента');
-    })
-
-    app.delete('/students', (req, res) => {
-        res.send('У студента');
+app.get('/advertisment_types', async (req, res) => {
+       
+    const { data, error } = await supabase
+  .from('advertisment_types')
+  .select()
+    res.send(data);
     })
 
 
